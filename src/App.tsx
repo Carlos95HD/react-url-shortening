7fd789feae54b06152e14f00e70892d115a92964
Carlos <hernandeasi7@gmail.com>
2022-04-08 01:15:43 -0300

    Created | Footer Component / Styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Shortener } from './components/shortener/Shortener';
 import { NavBar } from './components/layout/NavBar';
 import { Title } from './components/layout/Title';
 import { Statistics } from './components/layout/Statistics';
+import { Footer } from './components/layout/Footer';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Shortener />
         <Statistics />
       </div>
+      <Footer />
     </div>
   );
 }
