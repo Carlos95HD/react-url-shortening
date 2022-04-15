504901454696c0d5516fb963dc6ecc024ddf6f31
Carlos <hernandeasi7@gmail.com>
2022-04-15 01:19:41 -0300

    Updated | Navbar & Title

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -6,17 +6,16 @@ export const NavBar = () => {
   return (
     <nav className="border-gray-200 fixed top-0 z-50 w-full md:z-0 md:container md:mx-auto md:py-2.5 md:relative">
       <div className="flex flex-wrap items-center justify-between mx-auto text-center md:h-16">
-        <div className="bg-white py-2 px-6 flex w-full justify-between md:w-auto md:p-0">
+        <div className="bg-white pb-2 pt-4 px-6 flex w-full justify-between md:w-auto md:p-0">
           <h1 className="self-center text-2xl font-semibold whitespace-nowrap">
             Shortly
           </h1>
           <button
             data-collapse-toggle="mobile-menu"
             type="button"
-            className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden focus:outline-none "
+            className="inline-flex items-center ml-3 text-sm text-gray-500 rounded-lg md:hidden focus:outline-none "
             onClick={() => setOpenNav(!openNav)}
           >
-            <span className="sr-only">Open main menu</span>
             {openNav ? (
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -24,11 +23,11 @@ export const NavBar = () => {
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
-                stroke-width="2"
+                strokeWidth="2"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   d="M6 18L18 6M6 6l12 12"
                 />
               </svg>
@@ -39,11 +38,11 @@ export const NavBar = () => {
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
-                stroke-width="2"
+                strokeWidth="2"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   d="M4 6h16M4 12h16M4 18h16"
                 />
               </svg>
@@ -52,43 +51,45 @@ export const NavBar = () => {
         </div>
         <div
           className={`${
-            openNav ? "h-96 bg-nav p-8" : "h-0 overflow-hidden"
-          } w-full transition-all ease-out duration-500 mx-6 md:transition-none rounded-md md:p-0 md:bg-transparent md:flex md:justify-between md:grow md:ml-4 md:w-4/5 md:overflow-visible md:h-0`}
+            openNav ? "h-[23rem] bg-nav p-8" : "h-0 overflow-hidden"
+          } flex flex-col text-lg font-semibold text-white place-content-center w-full md:text-gray-400 transition-all ease-out duration-500 mx-6 md:flex-row md:transition-none rounded-md md:p-0 md:bg-transparent md:flex md:justify-between md:grow md:ml-4 md:w-4/5 md:overflow-visible md:h-0`}
         >
           {/* NavLinks */}
-          <ul className="flex flex-col mt-4 border-b border-gray-500 md:border-0 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:self-center">
+          <ul className="flex flex-col md:border-0 md:ml-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:self-center">
             <li className="self-center">
               <a
                 href="/"
-                className="block py-2 pr-4 pl-3 md:border-0 md:hover:text-gray-900 md:p-0"
+                className="block py-2 pr-4 pl-3 md:border-0 hover:text-gray-300 md:text-gray-violet md:hover:text-violet-dark md:p-0"
               >
                 Features
               </a>
             </li>
-            <li className="self-center">
+            <li className="self-center my-4">
               <a
                 href="/"
-                className="block py-2 pr-4 pl-3 md:border-0 md:hover:text-gray-900 md:p-0"
+                className="block py-2 pr-4 pl-3 md:border-0 hover:text-gray-300 md:text-gray-violet md:hover:text-violet-dark md:p-0"
               >
                 Pricing
               </a>
             </li>
-            <li className="self-center">
+            <li className="self-center mb-4 md:mb-0">
               <a
                 href="/"
-                className="block py-2 pr-4 pl-3 md:border-0 md:hover:text-gray-900 md:p-0"
+                className="block py-2 pr-4 pl-3 md:border-0 hover:text-gray-300 md:text-gray-violet md:hover:text-violet-dark md:p-0"
               >
                 Resources
               </a>
             </li>
           </ul>
 
+          <div className="border-b border-gray-500 sm:w-60 self-center md:hidden"></div>
+
           {/* Buttons */}
-          <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:self-center">
-            <li className="self-center">
+          <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:self-center md:w-[8.2rem]">
+            <li className="self-center mb-4 md:mb-0">
               <a
                 href="/"
-                className="block py-2 pr-4 pl-3 md:border-0 md:hover:text-gray-900 md:p-0"
+                className="block py-2 pr-4 pl-3 md:border-0 hover:text-gray-300 md:text-gray-violet md:hover:text-violet-dark md:p-0"
               >
                 Login
               </a>
@@ -96,7 +97,7 @@ export const NavBar = () => {
             <li>
               <a
                 href="/"
-                className="btn btn-primary block py-2 px-4 text-white rounded-full w-full md:w-auto md:border-0 md:hover:disabled:"
+                className="btn btn-primary block py-2 px-4 rounded-full w-full sm:w-60 md:w-auto md:border-0 md:hover:disabled"
               >
                 Sign Up
               </a>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js,tsx,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        gray:{
+          violet:'#9e9aa7'
+        },
+        violet:{
+          dark:'#232127'
+        }
+      },
+      fontSize:{
+        '4xl2':'2.5rem'
+      }
+    },
   },
   plugins: [],
 }
