21e6492413af1f82fcc6bda8768f226874bf91da
Carlos <hernandeasi7@gmail.com>
2022-05-19 01:19:05 -0300

    Updated | Fix Design Title Component

--- src/components/layout/Title.tsx
+++ src/components/layout/Title.tsx
@@ -4,15 +4,15 @@ export const Title = () => {
   return (
     <article className="bg-white container grid grid-row-2 mx-auto mt-24 md:grid-cols-2 overflow-visible">
       <article className="row-start-2 md:row-start-auto">
-        <div className="flex flex-col justify-center h-full items-center text-center md:text-left md:items-start md:w-3/4">
+        <div className="flex flex-col justify-center h-full items-center text-center md:text-left md:items-start">
           <h2 className="font-extrabold text-4xl lg:text-7xl">
             More than just shorter links
           </h2>
-          <p className="mt-4 mx-4 text-lg md:mx-0">
+          <p className="mt-4 mx-4 text-lg md:mx-0 md:w-3/4">
             Build your brand's recognition and get detailed insights on how yur
             links are performing.
           </p>
-          <button className="py-2 px-4 btn btn-primary mt-8 font-semibold rounded-full w-fit">
+          <button className="py-2 px-6 btn btn-primary mt-8 font-semibold rounded-full w-fit">
             Get Started
           </button>
         </div>
