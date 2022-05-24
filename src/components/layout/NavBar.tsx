6b86b673ffdd89cb4d03b751c017bd6e2b490758
Carlos <hernandeasi7@gmail.com>
2022-05-23 22:18:47 -0300

    Added | Link component

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import { Logo } from "./Logo";
 
 export const NavBar = () => {
@@ -8,9 +9,9 @@ export const NavBar = () => {
     <nav className="border-gray-200 fixed top-0 z-50 w-full md:z-0 md:container md:mx-auto md:py-2.5 md:relative">
       <div className="flex flex-wrap items-center justify-between mx-auto text-center md:flex-nowrap md:h-16">
         <div className="bg-white pb-2 pt-4 px-6 flex w-full justify-between md:w-auto md:p-0">
-          <a href="/#" className="self-center whitespace-nowrap">
+          <Link to="/#" className="self-center whitespace-nowrap">
             <Logo fill={"#232127"} />
-          </a>
+          </Link>
           <button
             data-collapse-toggle="mobile-menu"
             type="button"
@@ -58,28 +59,28 @@ export const NavBar = () => {
           {/* NavLinks */}
           <ul className="flex flex-col md:border-0 md:ml-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:self-center">
             <li className="self-center">
-              <a
-                href="/#"
+              <Link
+                to="/#"
                 className="block py-2 pr-4 pl-3 md:border-0 hover:text-gray-300 md:text-gray-violet md:hover:text-violet-dark md:p-0"
               >
                 Features
-              </a>
+              </Link>
             </li>
             <li className="self-center my-4">
-              <a
-                href="/#"
+              <Link
+                to="/#"
                 className="block py-2 pr-4 pl-3 md:border-0 hover:text-gray-300 md:text-gray-violet md:hover:text-violet-dark md:p-0"
               >
                 Pricing
-              </a>
+              </Link>
             </li>
             <li className="self-center mb-4 md:mb-0">
-              <a
-                href="/#"
+              <Link
+                to="/#"
                 className="block py-2 pr-4 pl-3 md:border-0 hover:text-gray-300 md:text-gray-violet md:hover:text-violet-dark md:p-0"
               >
                 Resources
-              </a>
+              </Link>
             </li>
           </ul>
 
@@ -88,20 +89,20 @@ export const NavBar = () => {
           {/* Buttons */}
           <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:self-center md:w-[8.2rem]">
             <li className="self-center mb-4 md:mb-0">
-              <a
-                href="/#"
+              <Link
+                to="/#"
                 className="block py-2 pr-4 pl-3 md:border-0 hover:text-gray-300 md:text-gray-violet md:hover:text-violet-dark md:p-0"
               >
                 Login
-              </a>
+              </Link>
             </li>
             <li>
-              <a
-                href="/#"
+              <Link
+                to="/#"
                 className="btn btn-primary block py-2 px-4 rounded-full w-full sm:w-60 md:w-auto md:border-0 md:hover:disabled"
               >
                 Sign Up
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
